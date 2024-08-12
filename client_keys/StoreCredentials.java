8a1b2f69e0ce52e7b90cb8a4f6cbefd6ead6032a
Sun0Jian <js12359@nyu.edu>
2024-08-12 00:02:39 -0400

    Update StoreCredentials.java

--- client_keys/StoreCredentials.java
+++ client_keys/StoreCredentials.java
@@ -2,8 +2,8 @@ package client_keys;
 
 public class StoreCredentials {
     public static void main(String[] args) {
-        String clientId = "jTzpVWGWrF8o3ikzD37No3PNzYciwQFX";
-        String clientSecret = "PyqprP1sgNl4njL6";
+        String clientId = "";
+        String clientSecret = "";
 
         try {
             EncryptionUtil.writeEncryptedDataToFile("src/client_id.txt", clientId);
