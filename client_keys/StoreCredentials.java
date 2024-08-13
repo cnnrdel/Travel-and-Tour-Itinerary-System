c59524feed9741645ef1b9165f881b81e0d503a7
Sun0Jian <js12359@nyu.edu>
2024-08-12 20:28:38 -0400

    app_id and app_secret are for bus and train

--- client_keys/StoreCredentials.java
+++ client_keys/StoreCredentials.java
@@ -2,12 +2,12 @@ package client_keys;
 
 public class StoreCredentials {
     public static void main(String[] args) {
-        String clientId = "";
-        String clientSecret = "";
+        String clientId = "3510aec5";
+        String clientSecret = "1e7f9a64a44f11db885af0c42dedc968";
 
         try {
-            EncryptionUtil.writeEncryptedDataToFile("src/client_id.txt", clientId);
-            EncryptionUtil.writeEncryptedDataToFile("src/client_secret.txt", clientSecret);
+            EncryptionUtil.writeEncryptedDataToFile("src/client_id2.txt", clientId);
+            EncryptionUtil.writeEncryptedDataToFile("src/client_secret2.txt", clientSecret);
             System.out.println("Credentials stored successfully.");
         } catch (Exception e) {
             e.printStackTrace();
